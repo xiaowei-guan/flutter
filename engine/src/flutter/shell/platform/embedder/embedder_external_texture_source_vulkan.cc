d285da42d43c552a5aecc7ef4953ce6ffee38d80
Xiaowei Guan <xiaowei.guan@samsung.com>
2026-07-20 22:44:33 +0800

    Add unit test

--- engine/src/flutter/shell/platform/embedder/embedder_external_texture_source_vulkan.cc
+++ engine/src/flutter/shell/platform/embedder/embedder_external_texture_source_vulkan.cc
@@ -43,6 +43,8 @@ EmbedderExternalTextureSourceVulkan::EmbedderExternalTextureSourceVulkan(
       impeller::vk::Image(reinterpret_cast<VkImage>(embedder_desc->image));
   texture_device_memory_ = impeller::vk::DeviceMemory(
       reinterpret_cast<VkDeviceMemory>(embedder_desc->image_memory));
+  destruction_callback_ = embedder_desc->destruction_callback;
+  user_data_ = embedder_desc->user_data;
 
   needs_yuv_conversion_ = RequiresYCBCRConversion(
       static_cast<impeller::vk::Format>(embedder_desc->format));
@@ -64,6 +66,12 @@ EmbedderExternalTextureSourceVulkan::EmbedderExternalTextureSourceVulkan(
 
   yuv_conversion_ = std::move(yuv_conversion);
   is_valid_ = true;
+}
+
+EmbedderExternalTextureSourceVulkan::~EmbedderExternalTextureSourceVulkan() {
+  if (destruction_callback_) {
+    destruction_callback_(user_data_);
+  }
 }
 
 impeller::PixelFormat ToPixelFormat(int32_t vk_format) {
