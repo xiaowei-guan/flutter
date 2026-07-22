bfc20fa2bc0a786cd5b739095d6f3fc8c29804b1
Jason Simmons <jsimmons@google.com>
2026-07-22 14:27:35 -0700

    code review feedback
    (cherry picked from commit 106f98f5685195048c926517493d161be22fbf98)

--- engine/src/flutter/shell/platform/embedder/embedder_surface_gl_impeller.cc
+++ engine/src/flutter/shell/platform/embedder/embedder_surface_gl_impeller.cc
@@ -218,6 +218,7 @@ sk_sp<GrDirectContext> EmbedderSurfaceGLImpeller::CreateResourceContext()
 
 // |EmbedderSurface|
 void EmbedderSurfaceGLImpeller::ReleaseResourceContext() const {
+  worker_->SetReactionsAllowedOnCurrentThread(false);
   gl_dispatch_table_.gl_clear_current_callback();
 }
 
